9b986140aaa4306ddd8cf88c88fe501f558ae264
Lincoln Ombelets <lombelets@caltech.edu>
2021-01-03 12:04:38 -0500

    ./build.sh at Sun 03 Jan 2021 12:04:38 PM EST

--- build.sh
+++ build.sh
@@ -1,19 +1,75 @@
 #!/usr/bin/bash
 
 LOGFILE="build.log"
+FILES="."
+MESSAGE="$0 at $(date)"
 
-echo "build.sh started at $(date)\n\n" | tee $LOGFILE
+usage() { 
+	echo <<EOF
+Usage: $0 [-d] [-b] [-m <message>] [-f <files>] [ -l <logfile> ]
+
+-d	dry-run, only build local copy, do not commit or deploy site.
+-b	build only, build and commit to main but do not deploy site.
+-m MESSAGE	supply a commit message for the main branch
+-f FILES	supply files to be committed to main; default .
+-l LOGFILE	specify a logfile name; default build.log
+EOF
+
+	exit 0;
+}
+
+usage()
+
+while getopts ":dbm:f:l:" OPTION; do
+	case $OPTION in
+	d)
+		NOCOMMIT=1
+		NODEPLOY=1
+		;;
+	b)
+		NODEPLOY=1
+		;;
+	m)
+		MESSAGE=$OPTARG
+		;;
+	f)
+		FILES=$OPTARG
+		;;
+	l)
+		LOGFILE=$OPTARG
+		;;
+	h)
+		usage
+		;;
+	*)
+		usage
+		;;
+	esac
+done
+
+
+echo "$0 started at $(date)\n\n" | tee $LOGFILE
 mkdocs build --site-dir build --verbose --strict &>> $LOGFILE
+
+if [[ $NOCOMMIT && $NODEPLOY ]]; then
+	echo "Local build complete and -d was specified, exiting." | tee -a $LOGFILE
+	exit 0;
+fi
 
 if [[ $? -eq 0 ]]; then
 	echo "\n\nTest build succeeded, committing to main \n\n" | tee -a $LOGFILE
 
-	git add . &>> $LOGFILE
-	git commit -m "build at $(date)" &>> $LOGFILE
+	git add $FILES &>> $LOGFILE
+	git commit -m "$MESSAGE" &>> $LOGFILE
 	git push origin main &>> $LOGFILE
 
+	if [[ $NODEPLOY ]]; then
+		echo "\n\n Pushed to main and -b was specified, exiting." | tee -a $LOGFILE
+		exit 0;
+	fi
+
 	echo "\n\nRunning mkdocs gh-deploy...\n\n" | tee -a $LOGFILE
-	mkdocs gh-deploy --verbose --strict &>> $LOGFILE
+	mkdocs gh-deploy --strict &>> $LOGFILE
 
 	if [[ $? -eq 0 ]]; then
 		echo "\n\nmkdocs gh-deploy succeeded, exiting.\n\n" | tee -a $LOGFILE

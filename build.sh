df8ccc1e3ab4ebd0711d9a610f36d697c04f84bd
Lincoln Ombelets <lombelets@caltech.edu>
2021-01-03 11:15:03 -0500

    build update

--- build.sh
+++ build.sh
@@ -2,28 +2,29 @@
 
 LOGFILE="build.log"
 
-echo "build.sh started at $(date)" | tee $LOGFILE
+echo "build.sh started at $(date)\n\n" | tee $LOGFILE
 mkdocs build --site-dir build --verbose --strict &>> $LOGFILE
 
 if [[ $? -eq 0 ]]; then
-	echo "Test build succeeded, committing to main" | tee -a $LOGFILE
+	echo "\n\nTest build succeeded, committing to main \n\n" | tee -a $LOGFILE
 
 	git add . &>> $LOGFILE
 	git commit -m "build at $(date)" &>> $LOGFILE
 	git push origin main &>> $LOGFILE
 
-	echo "Running mkdocs gh-deploy..." | tee -a $LOGFILE
+	echo "\n\nRunning mkdocs gh-deploy...\n\n" | tee -a $LOGFILE
 	mkdocs gh-deploy --verbose --strict &>> $LOGFILE
 
 	if [[ $? -eq 0 ]]; then
-		echo "mkdocs gh-deploy succeeded, exiting." | tee -a $LOGFILE
+		echo "\n\nmkdocs gh-deploy succeeded, exiting.\n\n" | tee -a $LOGFILE
+		exit 0
 	else
-		echo "mkdocs gh-deploy exited with nonzero status ${?}, see logfile ${LOGFILE}" | tee -a $LOGFILE
+		echo "\n\nmkdocs gh-deploy exited with nonzero status ${?}, see logfile ${LOGFILE}" | tee -a $LOGFILE
 		exit 1
 	fi
 
 else
 
-	echo "Test build exited with nonzero exit status, see logfile ${LOGFILE}" | tee -a $LOGFILE
+	echo "\n\nTest build exited with nonzero exit status, see logfile ${LOGFILE}" | tee -a $LOGFILE
 	exit 1
 fi

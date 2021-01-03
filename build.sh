b1628c1f9062d4318acb36a20665353bd2b25a52
Lincoln Ombelets <lombelets@caltech.edu>
2021-01-03 11:03:21 -0500

    build at Sun 03 Jan 2021 11:03:21 AM EST

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+LOGFILE="build.log"
+
+echo "build.sh started at $(date)" | tee $LOGFILE
+mkdocs build --site-dir build --verbose --strict >> $LOGFILE
+
+if [[ $? -eq 0 ]]; then
+	echo "Test build succeeded, committing to main" | tee -a $LOGFILE
+
+	git add .
+	git commit -m "build at $(date)"
+	git push origin main
+
+	echo "Running mkdocs gh-deploy..." | tee -a $LOGFILE
+	mkdocs gh-deploy --verbose --strict >> $LOGFILE
+
+	if [[ $? -eq 0 ]]; then
+		echo "mkdocs gh-deploy succeeded, exiting." | tee -a $LOGFILE
+	else
+		echo "mkdocs gh-deploy exited with nonzero status ${?}, see logfile ${LOGFILE}" | tee -a $LOGFILE
+		exit 1
+	fi
+
+else
+
+	echo "Test build exited with nonzero exit status, see logfile ${LOGFILE}" | tee -a $LOGFILE
+	exit 1
+fi
